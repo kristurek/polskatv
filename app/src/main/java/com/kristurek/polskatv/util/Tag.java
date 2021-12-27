e5b131cbf406cc67aad35ddd67a23c0c46d29998
Kris Turek <15099890+kristurek@users.noreply.github.com>
2021-12-27 13:45:46 -0600

    cleanup TAG

--- app/src/main/java/com/kristurek/polskatv/util/Tag.java
+++ app/src/main/java/com/kristurek/polskatv/util/Tag.java
@@ -4,6 +4,6 @@ public class Tag {
     public static final String SERVICE = "polskatv.service";
     public static final String UI = "polskatv.ui";
     public static final String DATA = "polskatv.data";
-    public static final String EVENT = "polskatv.ui.event";
-    public static final String MASSIVE = "polskatv.ui.massive";
+    public static final String EVENT = "polskatv.event";
+    public static final String MASSIVE = "polskatv.massive";
 }
